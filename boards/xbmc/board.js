686663f8a988f04e3aeb3ce003af5ddb789d16d9
datagutt <datagutt666@hotmail.com>
2012-12-23 20:11:34 +0100

    Fully working XBMC now playing

--- boards/xbmc/board.js
+++ boards/xbmc/board.js
@@ -1,24 +1,24 @@
 /* XbmcRequest taken and modified from https://github.com/blakeblackshear/xbmc-lib/blob/master/src/XbmcRequest.js */
-var XbmcRequest = function XbmcRequest(options) {
-	var host, postData, xhr, result = false;
-	xhr = new XMLHttpRequest();
-	host = options.settings.host !== null ? 'http://' + options.settings.host : '';
+var XbmcRequest = function XbmcRequest(options, el) {
+	var host, postData, result = false;
+	host = options.settings.host !== null ? 'ws://' + options.settings.host : '';
 	if(host && options.settings.port){
 		host += ':' + options.settings.port;
 	}
-	xhr.open('POST', '' + host + '/jsonrpc', true, options.settings.username, options.settings.password);
+	var websocket = new WebSocket(host + '/jsonrpc');
 	postData = {
 		jsonrpc: '2.0',
 		method: options.method,
 		params: options.params,
 		id: 1
 	};
-	xhr.send(JSON.stringify(postData));
+	websocket.onopen = function(){
+		websocket.send(JSON.stringify(postData));
+	};
 	if(options.callback != null){
-		xhr.onreadystatechange = function(){ 
-			if(xhr.readyState === 4){
-				result = options.callback(JSON.parse(xhr.responseText).request);
-			}
+		websocket.onmessage = function(evt){ 
+		console.log(evt.data);
+			result = options.callback(el, JSON.parse(evt.data));
 		};
 	}
 	return result;
@@ -31,24 +31,25 @@ XbmcBoard.prototype.init = function(el, config){
 		self.update(el, config.host, config.port);
 	}, 1 * 60 * 1000);
 }
-XbmcBoard.prototype.callback = function(data){
+XbmcBoard.prototype.callback = function(el, parsed){
 	var self = this;
 	var item, showtitle = '', season = 0, episode = 0, title = '';
-	var parsed = eval('(' + x + ')');
+	el.innerHTML = '<h2>XBMC</h2>';
 	if(parsed){
-		if(item = parsed.result.item){
+		if(parsed.result && parsed.result.item){
+			item = parsed.result.item;
 			showtitle = item.showtitle;
 			season = item.season;
 			episode = item.episode;
 			title = item.title;
 		}
 		if(showtitle && season && episode && title){
-			el.innerHTML = '<div id="nowplaying"><p>'+showtitle+'</p><p>'+season+'x'+episode+' '+title+'</div>';
+			el.innerHTML += '<div id="nowplaying"><p>'+showtitle+'</p><p>'+season+'x'+episode+' '+title+'</div>';
 		}else{
-			el.innerHTML = '<div id="nowplaying">Nothing playing</div>';
+			el.innerHTML += '<div id="nowplaying">Nothing playing</div>';
 		}
 	}else{
-		el.innerHTML = '<p>Can not load XBMC data</p>';
+		el.innerHTML += '<p>Can not load XBMC data</p>';
 	}
 }
 XbmcBoard.prototype.update = function(el, host, port){
@@ -60,11 +61,11 @@ XbmcBoard.prototype.update = function(el, host, port){
 		method: 'Player.GetItem',
 		settings: {
 			host: host,
-			port: port
+			port: port,
 		},
 		params: {
 			playerid: 1
 		},
 		callback: self.callback
-	});
+	}, el);
 }
